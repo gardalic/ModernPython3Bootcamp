cd485ae1ad06e75b5ca449b5b28cd1a1d8a2f144
Goran Ardalic <gardalic@iolap.com>
2020-10-28 09:00:24 +0100

    inheritance exercises

---
+++ exercises/start_oop/multiple_inheritance.py
@@ -0,0 +1,21 @@
+class Mother:
+    def __init__(self):
+        self.eye_color = "brown"
+        self.hair_color = "dark brown"
+        self.hair_type = "curly"
+
+
+class Father:
+    def __init__(self):
+        self.eye_color = "blue"
+        self.hair_color = "blond"
+        self.hair_type = "straight"
+
+
+class Child(Mother, Father):
+    pass
+
+
+c1 = Child()
+
+print(c1.eye_color)
