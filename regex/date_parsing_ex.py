6563d80b0b10e14926e65fa21cc9573906908a5d
gardalic <gardalic@gmail.com>
2020-12-28 16:09:33 +0100

    Regex profanity filter exercise

--- regex/date_parsing_ex.py
+++ regex/date_parsing_ex.py
@@ -1,17 +1,18 @@
-"""
-Define a function called parse_date  that accepts a single string.  Your code should check to see if the string matches a date format.  
-We're going to use the DMY format of "dd/mm/yyyy", but it should also work with "dd.mm.yyyy" and "dd,mm,yyyy". If you are American, note that Day if before Month!  
-However, rather than simply returning True or False if the string matches...you should instead return a dictionary containing the three parts of the date with 
-the keys "d", "m" , and "y"
-"""
+"""Define a function called parse_date  that accepts a single string.  Your code should check to see if the string
+matches a date format. We're going to use the DMY format of "dd/mm/yyyy", but it should also work with "dd.mm.yyyy"
+and "dd,mm,yyyy". If you are American, note that Day if before Month! However, rather than simply returning True or
+False if the string matches...you should instead return a dictionary containing the three parts of the date with the
+keys "d", "m" , and "y" """
 import re
 
-def parse_date(input):
+
+def parse_date(date_in):
     pattern = re.compile(r"^([0-9]{2})[.,/]([0-9]{2})[.,/]([0-9]{4})$")
-    match = pattern.search(input)
+    match = pattern.search(date_in)
     if match:
         return {"d": match.group(1), "m": match.group(2), "y": match.group(3)}
     return None
+
 
 print(parse_date("12.11.2003"))
 print(parse_date("12,04,2003"))
---
+++ regex/regex_profanity_filter.py
@@ -0,0 +1,24 @@
+"""
+    Define a function called censor  that accepts a single string. Rather than censoring any real
+    profanity, we're going to censor any words that start with "frack".  This includes "fracking", "fracker",
+    "frack", etc.  Replace the entire word with the string "CENSORED".  Your regex should be case insensitive.
+    For example:
+        censor("Frack you") # CENSORED you
+        censor("I hope you fracking die") # I hope you CENSORED die
+        censor("you fracking Frack") # you CENSORED CENSORED
+"""
+import re
+
+
+def censor(text) -> str:
+    pattern = re.compile(r"\bfrack\w*\b", re.IGNORECASE)
+    # match = re.findall(pattern, text)
+    if re.findall(pattern, text):
+        return re.sub(pattern, "CENSORED", text)
+    else:
+        return text
+
+
+print(censor("Frack you"))  # CENSORED you
+print(censor("I hope you fracking die")) # I hope you CENSORED die
+print(censor("you fracking Frack")) # you CENSORED CENSORED
